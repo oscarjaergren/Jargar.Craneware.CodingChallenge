d93ccc291a8e9e2fd50b642e2166ceff2b90a35f
Oscar Järgren <31920401+oscarjaergren@users.noreply.github.com>
2023-11-13 19:21:17 +0000

    Implement tests and its edge cases

---
+++ Source/ReverseString/ReversingString.Tests/StringUtilitiesTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace CodingChallenge.ReversingString.Tests;
+
+[TestFixture]
+public class StringUtilitiesTests
+{
+    [TestCase("FooBazQux", "xuQzaBooF")]
+    [TestCase("Hello Bar", "raB olleH")]
+    [TestCase("A", "A")]
+    [TestCase("123!@#", "#@!321")]
+    [TestCase("Unicode测试", "试验eciuDnoU")]
+    public void Reverse_WhenCalledWithValidInput_ShouldReturnReversedString(string input, string expectedOutput)
+    {
+        string result = StringUtilities.Reverse(input);
+        Assert.AreEqual(expectedOutput, result);
+    }
+
+    [TestCase(null, "")]
+    public void Reverse_WhenCalledWithNullInput_ShouldReturnEmptyString(string input, string expectedOutput)
+    {
+        string result = StringUtilities.Reverse(input);
+        Assert.AreEqual(expectedOutput, result);
+    }
+
+    [Test]
+    public void Reverse_WhenCalledWithWhitespaceOnly_ShouldPreserveWhitespace()
+    {
+        const string input = " \t\n\r\f\v";
+        string result = StringUtilities.Reverse(input);
+        Assert.AreEqual(input, result);
+    }
+}
