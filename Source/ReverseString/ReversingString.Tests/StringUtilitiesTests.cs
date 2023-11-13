2865465238e4f6faa7ed44ca580d305de796ac74
Oscar Järgren <31920401+oscarjaergren@users.noreply.github.com>
2023-11-13 23:36:42 +0000

    Fix tests

--- Source/ReverseString/ReversingString.Tests/StringUtilitiesTests.cs
+++ Source/ReverseString/ReversingString.Tests/StringUtilitiesTests.cs
@@ -5,21 +5,29 @@ namespace CodingChallenge.ReversingString.Tests;
 [TestFixture]
 public class StringUtilitiesTests
 {
+    readonly IStringUtilities _stringUtilities;
+
+    public StringUtilitiesTests()
+    {
+        //Ideally would be constructor injected intead
+        _stringUtilities = new StringUtilities();
+    }
+
     [TestCase("FooBazQux", "xuQzaBooF")]
     [TestCase("Hello Bar", "raB olleH")]
     [TestCase("A", "A")]
     [TestCase("123!@#", "#@!321")]
-    [TestCase("Unicode测试", "试验eciuDnoU")]
+    [TestCase("Unicode测试", "试测edocinU")]
     public void Reverse_WhenCalledWithValidInput_ShouldReturnReversedString(string input, string expectedOutput)
     {
-        string result = StringUtilities.Reverse(input);
+        string result = _stringUtilities.ReverseString(input);
         Assert.AreEqual(expectedOutput, result);
     }
 
-    [TestCase(null, "")]
+    [TestCase(null, null)]
     public void Reverse_WhenCalledWithNullInput_ShouldReturnEmptyString(string input, string expectedOutput)
     {
-        string result = StringUtilities.Reverse(input);
+        string result = _stringUtilities.ReverseString(input);
         Assert.AreEqual(expectedOutput, result);
     }
 
@@ -27,7 +35,7 @@ public class StringUtilitiesTests
     public void Reverse_WhenCalledWithWhitespaceOnly_ShouldPreserveWhitespace()
     {
         const string input = " \t\n\r\f\v";
-        string result = StringUtilities.Reverse(input);
+        string result = _stringUtilities.ReverseString(input);
         Assert.AreEqual(input, result);
     }
 }
