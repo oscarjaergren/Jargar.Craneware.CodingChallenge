c7dc044357ab4f088d282eec54e7f9363e4bae1a
Oscar Järgren <31920401+oscarjaergren@users.noreply.github.com>
2023-11-14 15:43:46 +0000

    Add comment for simpleshuffle

--- Source/CardShuffle/CardGame/Shuffles/SimpleShuffle.cs
+++ Source/CardShuffle/CardGame/Shuffles/SimpleShuffle.cs
@@ -5,6 +5,7 @@ namespace CodingChallenge.CardGame.Shuffles;
 
 internal class SimpleShuffle : IShuffle
 {
+    //Only available in .net 8, which might be available by the time I submit this code
     public void Shuffle(ref List<ICard> cards)
     {
         //  Random random = new();
