744f2a406b2c763ce0fac17fd56d85904d49c921
Oscar Järgren <31920401+oscarjaergren@users.noreply.github.com>
2023-11-14 15:45:13 +0000

    Added ReverseStrings Benchmarks to test the various implementations

---
+++ Source/ReverseString/ReverseString.Benchmarks/StringReverseBenchmarks.cs
@@ -0,0 +1,41 @@
+﻿using BenchmarkDotNet.Attributes;
+using CodingChallenge.ReversingString;
+
+namespace ReverseString.Benchmarks;
+
+// Benchmarks cannot be static
+#pragma warning disable CA1822 // Mark members as static
+
+[MemoryDiagnoser]
+public class StringReverseBenchmarks
+{
+    [Benchmark]
+    public void ToCharArrayStringReverseBenchmark()
+    {
+        var implementation = new ToCharArrayStringReverse();
+        ExecuteBenchmark(implementation);
+    }
+
+    [Benchmark]
+    public void StringBuilderReverseBenchmark()
+    {
+        var implementation = new StringBuilderReverse();
+        ExecuteBenchmark(implementation);
+    }
+
+    [Benchmark]
+    public void LinqStringReverseBenchmark()
+    {
+        var implementation = new LinqStringReverse();
+        ExecuteBenchmark(implementation);
+    }
+
+    private static void ExecuteBenchmark(IStringUtilities implementation)
+    {
+        const string input = "BenchmarkTest";
+        string result = implementation.ReverseString(input);
+        Console.WriteLine(result); // To prevent optimizations from removing the code
+    }
+}
+
+#pragma warning restore CA1822 // Mark members as static
